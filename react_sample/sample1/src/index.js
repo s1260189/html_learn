19f2291595077a6803ad2cd9354d2765d0c4f726
Yuta Yoneyama <s1260189@u-aizu.ac.jp>
2021-11-16 19:57:25 +0900

    TTT

--- react_sample/sample1/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SampleButton from "./SampleButton";
-document.addEventListener("DOMContentLoaded", function(){
- ReactDOM.render(<SampleButton />, document.getElementById("root"));
-});
